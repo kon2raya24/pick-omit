fa6f8c79cc47ef5b4d777ebfea43573b0cc6a336
kon2raya24 <konturaya@gmail.com>
2026-06-25 13:42:32 +0000

    docs: add JSDoc comments to pick.ts

--- src/pick.ts
+++ src/pick.ts
@@ -1,3 +1,8 @@
+/**
+ * pick<T extends Record<string, any>, K extends keyof T>
+ * @param obj
+ * @param keys
+ */
 export function pick<T extends Record<string, any>, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> {
   const result = {} as Pick<T, K>;
   for (const key of keys) {
